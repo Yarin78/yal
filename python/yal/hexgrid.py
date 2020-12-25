00fd9404a9a1e3424ab0e87e775755690041e7c1
Jimmy Mårdell <jimmy.mardell@gmail.com>
2020-12-25 14:29:19 +0100

    Added hexgrid

--- python/yal/hexgrid.py
+++ python/yal/hexgrid.py
@@ -2,6 +2,8 @@ from yal.geo2d import Point
 
 # https://www.redblobgames.com/grids/hexagons/
 
+
+# Cube coordinates, using x,z
 HEX_DIRS_EW = {
     'e': Point(1, 0),
     'w': Point(-1, 0),
@@ -9,4 +11,14 @@ HEX_DIRS_EW = {
     'sw': Point(-1, 1),
     'nw': Point(0, -1),
     'ne': Point(1, -1),
+}
+
+# Cube coordinate; as above rotate 90 degrees ccw, using z, x (Not tested!)
+HEX_DIRS_NS = {
+    'n': Point(0, -1),
+    's': Point(0, 1),
+    'ne': Point(-1, 0),
+    'nw': Point(1, -1),
+    'sw': Point(1, 0),
+    'se': Point(-1, 1),
 }
